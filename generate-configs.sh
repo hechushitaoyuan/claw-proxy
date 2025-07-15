5235ba2ce115beea7d67b420610c132fea524c43
Wu Guo <109302248+hechushitaoyuan@users.noreply.github.com>
2025-07-15 23:24:23 +0800

    Update generate-configs.sh

--- generate-configs.sh
+++ generate-configs.sh
@@ -13,41 +13,31 @@ SERVICE_LOCATIONS=""
 while read -r service_prefix; do
   service_prefix=$(echo -n "$service_prefix" | tr -d '\r')
 
-  if [ -z "$service_prefix" ] || [ "${service_prefix#\#}" != "$service_prefix" ]; then
+  if [ -z "$service_prefix" ]; then
     continue
   fi
 
   echo "Generating location block for: $service_prefix"
   
-  # 【最终决战·分流处理】
-  # 我们只对gb和npm这两个有问题的应用，启用“内容替换”超能力
-  # 对于gp这个本身就很现代化的应用，我们保持原样
-  SUB_FILTER_CONFIG=""
-  if [ "$service_prefix" = "gb" ] || [ "$service_prefix" = "npm" ]; then
-    SUB_FILTER_CONFIG="
-        # 开启内容替换
-        sub_filter_once off;
-        # 把 href=\"/ 替换成 href=\"/${service_prefix}/
-        sub_filter 'href=\"/' 'href=\"/${service_prefix}/';
-        # 把 src=\"/ 替换成 src=\"/${service_prefix}/
-        sub_filter 'src=\"/' 'src=\"/${service_prefix}/';
-        # 把 action=\"/ 替换成 action=\"/${service_prefix}/
-        sub_filter 'action=\"/' 'action=\"/${service_prefix}/';
-    "
-  fi
-
   SERVICE_LOCATIONS="${SERVICE_LOCATIONS}
+    # 【终极决战·路径重写】
+    # 匹配所有以 /gb/ 开头的请求
     location /${service_prefix}/ {
-        proxy_pass http://${service_prefix}.${BASE_DOMAIN}/;
-        proxy_set_header Host ${service_prefix}.${BASE_DOMAIN};
+        # 【关键】告诉Nginx，在将请求发给后端前，
+        # 把URL中的 /gb/ 部分砍掉，只保留后面的部分。
+        # 例如：/gb/auth 会被重写为 /auth
+        rewrite ^/${service_prefix}/(.*)$ /\$1 break;
         
         # --- 之前的所有配置都保持完美 ---
-        proxy_redirect http://${service_prefix}.${BASE_DOMAIN}/ /${service_prefix}/;
-        proxy_redirect https://${service_prefix}.${BASE_DOMAIN}/ /${service_prefix}/;
-        proxy_set_header Accept-Encoding \"\"; # 【关键】禁用压缩，sub_filter才能生效
+        proxy_pass http://${service_prefix}.${BASE_DOMAIN};
+        proxy_set_header Host ${service_prefix}.${BASE_DOMAIN};
         
-        # 【加载超能力】
-        ${SUB_FILTER_CONFIG}
+        # 【关键】我们不再需要sub_filter，所以把Accept-Encoding加回来，恢复压缩
+        # proxy_set_header Accept-Encoding \"\"; 
+        
+        # 【关键】因为路径已经被重写，后端的任何相对路径重定向都会是正确的！
+        # 我们只需要一个更通用的proxy_redirect
+        proxy_redirect / /${service_prefix}/;
 
         proxy_set_header X-Real-IP \$remote_addr;
         proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
@@ -67,7 +57,7 @@ server {
 
     location = / {
         add_header Content-Type text/plain;
-        return 200 "jiamian personal domain";
+        return 200 "jiamian personal domain is running~~";
     }
 
     ${SERVICE_LOCATIONS}
